ed07990081e17f1991e9369bfd9ec9bd3971fcaf
felipeffranco <felipeffranco@outlook.com>
2023-10-17 15:39:58 -0300

    reverse string done with reverse method in javascript

---
+++ daily-1-reverse-string/index.js
@@ -0,0 +1,8 @@
+function reverseString(string) {
+    const reverse = [...string].reverse()
+    console.log(reverse)  
+}
+
+reverseString("Cat");
+reverseString("The Daily Byte");
+reverseString("civic");
